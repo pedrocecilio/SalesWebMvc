09625a6cd89871a05580c973f69fd635d7fc69ef
pedrocecilio <pedrox.cecilio@gmail.com>
2022-01-04 00:06:19 -0300

    Finalização do projeto

---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        //Contrutor sem argumentos
+        public Departament()
+        {
+        }
+
+        //Construtor com argumentos
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //Adiciona o vendedor
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        //Retorna total de vendas 
+        public double TotalSales(DateTime inittial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inittial, final));
+        }
+    }
+}
---
+++ SalesWebMvc/Services/DepartamentService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartamentService
+    {
+        //Crio dependencia
+        private readonly SalesWebMvcContext _context;
+
+        //Crio injeção de pendencia
+        public DepartamentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        //Retorno lista de departamentos
+        public async Task<List<Departament>> FindAllAsync()
+        {
+            return await _context.Departament.OrderBy(x=>x.Name).ToListAsync();
+        }
+    }
+}
